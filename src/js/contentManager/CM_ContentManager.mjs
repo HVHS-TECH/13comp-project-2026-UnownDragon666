1f1179ce9751e40e54fc0f1f5dc3676be39c1912
UnownDragon666 <unown.idrees@gmail.com>
2026-02-26 13:26:53 +1300

    Content manager files

---
+++ src/js/contentManager/CM_ContentManager.mjs
@@ -0,0 +1,21 @@
+/*
+    CM_ContentManager.mjs
+    
+    Content Manager is a class made to manage the contents
+    of the HTML page in this project.
+
+
+*/
+
+export class ContentManager {
+    // Private Fields
+
+    // Public Fields
+
+    // Constructor
+    ContentManager() {
+
+    }
+
+
+}
